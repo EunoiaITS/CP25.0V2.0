776fe770621982fbeb7a163721116c7e70e2dbcd
Sagar-EIS <sagar.biswas@eunoiaits.com>
2019-01-09 21:59:28 +0600

    Fixed home homepage link

--- resources/views/admin/navigation.blade.php
+++ resources/views/admin/navigation.blade.php
@@ -7,7 +7,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="index.html">LOGO</a>
+        <a class="navbar-brand" href="/">Naisse</a>
     </div>
     <!-- /.navbar-header -->
 
